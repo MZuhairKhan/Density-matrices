c85fc06964aec5d63453e418d9a6f2c1e37e75eb
Zuhair Khan <khanmohammadzuhair@gmail.com>
2024-01-15 17:38:44 +0200

    Fixed partial_traces

--- src/density_matrix.py
+++ src/density_matrix.py
@@ -1,52 +1,41 @@
+from __future__ import annotations
+
+from collections.abc import Iterable
+
 import numpy as np
 
 
 class DensityMatrix:
-    @staticmethod
-    def trace(matrix):
-        """
-        Calculate the trace of a given matrix.
-
-        Parameters:
-            matrix (numpy.ndarray): The input matrix for which the trace needs to be calculated.
-
-        Returns:
-            float: The trace of the matrix.
-        """
-        return np.trace(matrix)
-
-    def __init__(self, data):
+    def __init__(self, data) -> None:
         """
         Initializes the object with the given data.
 
         Parameters:
-            data (array-like): The input data to initialize the object. It must be convertible to a numpy array.
+            data (array-like):  The input data to initialize the object.
+                                It must be convertible to a numpy array.
 
         Raises:
-            ValueError: If the input data cannot be converted to a numpy array or if it is not an array of complex numbers.
-
-        Returns:
-            None
+            ValueError: If the input data cannot be converted to a numpy array or if
+                        it is not an array of complex numbers.
         """
         try:
-            array = np.asarray(data)
+            array = np.asarray(data, dtype=complex)
         except ValueError:
-            raise ValueError("Input must be convertible to a numpy array.")
+            raise ValueError(
+                "Input must be convertible to a numpy array of complex numbers."
+            )
 
-        if not np.iscomplexobj(array):
-            try:
-                array = array.astype(complex)
-            except:
-                raise ValueError("Input must be an array of complex numbers.")
+        if array.ndim not in {1, 2}:
+            raise ValueError(
+                f"Input must be a 1D or 2D array, got {array.ndim}D array."
+            )
 
         if array.ndim == 1:
-            self.initialize_from_statevector(array)
+            self._initialize_from_statevector(array)
         elif array.ndim == 2:
-            self.initialize_from_matrix(array)
-        else:
-            raise ValueError("Input must be a 1D or 2D array.")
+            self._initialize_from_matrix(array)
 
-    def initialize_from_statevector(self, statevector):
+    def _initialize_from_statevector(self, statevector: np.ndarray) -> None:
         """
         Initializes the quantum circuit from a given state vector.
 
@@ -54,77 +43,124 @@ class DensityMatrix:
             statevector (np.ndarray): The state vector to initialize the circuit from.
 
         Raises:
-            ValueError: If the state vector is empty or if its dimensions are not a power of 2.
-
-        Returns:
-            None
+            ValueError: If the state vector is empty.
         """
         if statevector.size == 0:
             raise ValueError("Statevector must not be empty.")
-        if not self._is_power_of_two(statevector.size):
-            raise ValueError("Statevector dimensions must be a power of 2.")
-        self.initialize_from_matrix(np.outer(statevector, statevector.conj()))
+        self._initialize_from_matrix(np.outer(statevector, statevector.conj()))
 
-    def initialize_from_matrix(self, matrix):
+    def _initialize_from_matrix(self, matrix: np.ndarray) -> None:
         """
-        Initializes the density matrix from a given matrix.
+        Initialize the object from a given matrix.
 
         Parameters:
-            matrix (ndarray): The matrix representing the density matrix.
+            matrix (np.ndarray): The matrix to initialize from.
+        """
+        self._validate_matrix(matrix)
+        self._matrix = matrix / np.trace(matrix)
+        self._dimension = self._calculate_square_matrix_dimension(matrix)
+        self._purity = np.trace(self._matrix @ self._matrix)
+
+    def _validate_matrix(self, matrix: np.ndarray) -> None:
+        """
+        Validates a given matrix to ensure it meets certain criteria.
+
+        Parameters:
+            matrix (np.ndarray): The matrix to be validated.
 
         Raises:
-            ValueError: If the matrix is empty, not square, dimensions are not a power of 2,
-                        or if the matrix has a trace of zero.
+            ValueError: If the matrix is empty or not square.
             ValueError: If the matrix is not Hermitian.
             ValueError: If the matrix is not positive semidefinite.
-
-        Returns:
-            None
+            ValueError: If the matrix has a trace of zero.
         """
         if matrix.size == 0:
             raise ValueError("Density matrices must not be empty.")
         if matrix.shape[0] != matrix.shape[1]:
             raise ValueError("Density matrices must be square.")
+        self._validate_square_matrix(matrix)
+        self._validate_hermitian(matrix)
+        self._validate_positive_semidefinite(matrix)
+
+    def _validate_square_matrix(self, matrix: np.ndarray) -> None:
+        """
+        Validates the dimensions of a square matrix.
+
+        Parameters:
+            matrix (np.ndarray): The square matrix to validate.
+
+        Raises:
+            ValueError: If the dimensions of the matrix are not a power of 2
+                        or if the matrix has a trace of zero.
+        """
         if not self._is_power_of_two(matrix.shape[0]):
             raise ValueError(
-                "Density matrices must have dimensions that are a power of 2."
+                "Density matrices must have dimensions that are a power of 2, "
+                + f"got {matrix.shape[0]}."
             )
-        if self.trace(matrix) == 0:
+        if np.trace(matrix) == 0:
             raise ValueError("Cannot normalize a matrix with a trace of zero.")
-        matrix = matrix / self.trace(matrix)
+
+    def _validate_hermitian(self, matrix: np.ndarray) -> None:
+        """
+        Validate whether a given matrix is Hermitian.
+
+        Parameters:
+            matrix (np.ndarray): The matrix to be validated.
+
+        Raises:
+            ValueError: If the matrix is not Hermitian.
+        """
         if not np.allclose(matrix, matrix.conj().T):
             raise ValueError("Density matrices must be Hermitian.")
+
+    def _validate_positive_semidefinite(self, matrix: np.ndarray) -> None:
+        """
+        Validate whether a given matrix is positive semidefinite.
+
+        Parameters:
+            matrix (np.ndarray): The matrix to be validated.
+
+        Raises:
+            ValueError: If the matrix is not positive semidefinite.
+        """
         if np.any(np.linalg.eigvalsh(matrix) < 0):
             raise ValueError("Density matrices must be positive semidefinite.")
-        self.matrix = matrix
 
-    def purity(self):
+    def _calculate_square_matrix_dimension(self, matrix: np.ndarray) -> int:
+        """
+        Quickly calculates the dimension of a square matrix if the size is known
+        to be a power of 2.
+
+        Parameters:
+            matrix (np.ndarray): The square matrix for which the dimension needs
+                                 to be calculated.
+
+        Returns:
+            int: The dimension of the square matrix.
+        """
+        return matrix.shape[0].bit_length() - 1
+
+    def purity(self) -> float:
         """
         Calculates the purity of the density matrix.
 
         Returns:
             float: The purity value of the density matrix.
-
-        Raises:
-            ValueError: If the density matrix is not unitary.
         """
-        if not hasattr(self, "_purity"):
-            self._purity = DensityMatrix.trace(self.matrix @ self.matrix)
-            if self._purity > 1:
-                raise ValueError("The density matrix is not unitary.")
         return self._purity
 
-    def is_pure(self):
+    def is_pure(self) -> bool:
         """
         Determines if the object is pure.
 
         Returns:
             bool: True if the object is pure, False otherwise.
         """
-        return self.purity() == 1
+        return np.isclose(self.purity(), 1)
 
     @staticmethod
-    def _is_power_of_two(n):
+    def _is_power_of_two(n: int) -> bool:
         """
         Determines if a given number is a power of two.
 
@@ -136,16 +172,79 @@ class DensityMatrix:
         """
         return (n & (n - 1)) == 0 and n != 0
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         """
         Return a string representation of the DensityMatrix object.
-
-        Parameters:
-            self (DensityMatrix): The DensityMatrix object.
 
         Returns:
             str: A string representation of the DensityMatrix object.
         """
-        rows = [" ".join(str(element) for element in row) for row in self.matrix]
-        matrix_str = "\n".join(rows)
-        return f"DensityMatrix(\n{matrix_str}\n)"
+        sep = "  "
+        rows = [
+            sep.join("{0.real:.1f}+{0.imag:.1f}j".format(element) for element in row)
+            for row in self.matrix()
+        ]
+        matrix_str = ("\n" + sep).join(rows)
+        return "DensityMatrix(\n" + sep + f"{matrix_str}\n)"
+
+    def dimension(self) -> int:
+        """
+        Return the number of dimensions of the DensityMatrix object.
+
+        Returns:
+            int: The number of dimensions of the DensityMatrix object.
+        """
+        return self._dimension
+
+    def matrix(self) -> np.ndarray:
+        """
+        Return the matrix of the DensityMatrix object.
+
+        Returns:
+            np.ndarray: The matrix of the DensityMatrix object.
+        """
+        return self._matrix
+
+    def partial_trace(self, subsystems_to_trace_out) -> DensityMatrix:
+        """
+        Calculates the partial trace of the density matrix with respect to the given
+        subsystems. The subsystems are either given as an integer or as an iterable
+        of integers of the subsystems.
+
+        Parameters:
+            subsystems_to_trace_out (int or iterable):
+                The subsystem indices to trace out.
+
+        Returns:
+            DensityMatrix: The density matrix after performing the partial trace.
+        """
+        if isinstance(subsystems_to_trace_out, int):
+            subsystems_to_trace_out = [subsystems_to_trace_out]
+
+        try:
+            subsystems_to_trace_out = np.asarray(subsystems_to_trace_out, dtype=int)
+        except ValueError:
+            raise ValueError(
+                "The subsystems to trace out must be an integer or an iterable."
+            )
+
+        density_matrix = self.matrix()
+
+        for subsystem_index in subsystems_to_trace_out:
+            dimensions = self._calculate_square_matrix_dimension(density_matrix)
+            subsystem_index = subsystem_index - 1
+            if dimensions <= subsystem_index or subsystem_index < 0:
+                raise ValueError("The subsystem index is out of bounds.")
+
+            new_shape = [2] * (2 * dimensions)
+            matrix_reshaped = density_matrix.reshape(new_shape)
+
+            axis_to_sum = (subsystem_index, subsystem_index + dimensions)
+            partial_matrix = np.trace(
+                matrix_reshaped, axis1=axis_to_sum[0], axis2=axis_to_sum[1]
+            )
+
+            final_shape = [2] * (2 * dimensions - 2)
+            density_matrix = partial_matrix.reshape(final_shape)
+
+        return DensityMatrix(density_matrix)
